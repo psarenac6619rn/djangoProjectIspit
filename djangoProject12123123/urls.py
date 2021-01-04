b7e0d90ecd3464b472d187125d06f7a0f74a6ce6
psarenac6619rn <psarenac6619rn@raf.rs>
2021-01-05 00:45:53 +0100

    migracija

--- djangoProject12123123/urls.py
+++ djangoProject12123123/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
-from django.http import HttpResponse
-
-def home(request):
-    return  HttpResponse('Home page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home)
+    path('', include('projekat.urls'))
 ]
