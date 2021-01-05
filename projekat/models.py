1e51d8b9222cc9d6c0a2852c4bd7e4eef1bfad3a
psarenac6619rn <psarenac6619rn@raf.rs>
2021-01-05 03:51:58 +0100

    models

--- projekat/models.py
+++ projekat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Stolica(models.Model):
+    model = models.CharField(max_length=100)
+    dimenzije = models.CharField(max_length=50)
+    cena_DIN = models.IntegerField(default=0)
+    proizvedeno_Od = models.ForeignKey(Drvo, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.naziv
+
+class Drvo(models.Model):
+    vrsta = models.CharField(max_length=50)
+    cena_DIN = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.naziv
