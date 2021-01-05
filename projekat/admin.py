8ca191e24b691b318e21769880672c6fccef2d4a
psarenac6619rn <psarenac6619rn@raf.rs>
2021-01-05 04:09:59 +0100

    Tabela radi

--- projekat/admin.py
+++ projekat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Drvo, Stolica
+
+admin.site.register(Drvo)
+admin.site.register(Stolica)
--- projekat/models.py
+++ projekat/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+class Drvo(models.Model):
+    vrsta = models.CharField(max_length=50)
+    cena_DIN = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.naziv
+
 class Stolica(models.Model):
     model = models.CharField(max_length=100)
     dimenzije = models.CharField(max_length=50)
     cena_DIN = models.IntegerField(default=0)
     proizvedeno_Od = models.ForeignKey(Drvo, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return self.naziv
-
-class Drvo(models.Model):
-    vrsta = models.CharField(max_length=50)
-    cena_DIN = models.IntegerField(default=0)
 
     def __str__(self):
         return self.naziv
